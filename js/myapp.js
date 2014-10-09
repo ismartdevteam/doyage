09c14be1751eada6245c998c82dd96411a516b71
ismartdevteam <ulziiburend@gmail.com>
2014-10-09 19:38:44 +0800

    slider and menu

--- js/myapp.js
+++ js/myapp.js
@@ -2,11 +2,15 @@ app = angular.module("MyApp", ["ngRoute"]);
 app.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.
-                when('/HOME', {
+                when('/Home', {
                     templateUrl: 'view/home.html',
                     controller: 'HomeCtrl'
                 }).
-                when('/TOURS', {
+                when('/Mission', {
+                    templateUrl: 'view/mission.html',
+                    controller: 'HomeCtrl'
+                }).
+                when('/Tours', {
                     templateUrl: 'view/tour.html',
                     controller: 'TourCtrl'
                 }).
@@ -15,29 +19,30 @@ app.config(['$routeProvider',
                     controller: 'StoriesCtrl'
                 }).
                 otherwise({
-                    redirectTo: '/HOME'
+                    redirectTo: '/Home'
                 });
+               
     }]);
-app.controller("StoriesCtrl", function($http, $scope,$routeParams) {
+app.controller("StoriesCtrl", function($http, $scope, $routeParams) {
     AA();
-$scope.data=$routeParams.ID;
-    $scope.ArrayString = ["ss","aa","qq","ww"];
-    $scope.MyFFunc = function(dat){
-        $(".AAFunc[myid='"+aa+"']").css("font-size","50px");
-        
+    $scope.data = $routeParams.ID;
+    $scope.ArrayString = ["ss", "aa", "qq", "ww"];
+    $scope.MyFFunc = function(dat) {
+        $(".AAFunc[myid='" + aa + "']").css("font-size", "50px");
+
     };
-    $scope.GG = function(id){
-        if(id === 1){
+    $scope.GG = function(id) {
+        if (id === 1) {
             $(".BodyMM").css({
-                "background-color":"red",
-                height:"40%"
+                "background-color": "red",
+                height: "40%"
             });
             aa = $(".BodyMM").css("background-color");
             console.log(aa);
-        }else{
+        } else {
             $(".BodyMM").css({
-                "background-color":"blue",
-                height:"70%"
+                "background-color": "blue",
+                height: "70%"
             });
         }
     };
@@ -50,13 +55,13 @@ app.controller("HomeCtrl", function($http, $scope) {
     $scope.data = "hha";
 
 });
-app.controller("TourCtrl", function($scope,$routeParams) {
+app.controller("TourCtrl", function($scope, $routeParams) {
     $scope.data = $routeParams.ID;
 });
 app.controller("HeaderCtrl", function($http, $scope) {
     $scope.HeaderInfo = [
         {
-            name: "Visions",
+            name: "misions",
             id: 1
         },
         {
@@ -75,14 +80,14 @@ app.controller("HeaderCtrl", function($http, $scope) {
 });
 
 
-function AA(){
-    $("body").on("mouseenter",".AAFunc",function(){
+function AA() {
+    $("body").on("mouseenter", ".AAFunc", function() {
         $(this).css({
-            "color":"black",
-            "font-size":"16px"
+            "color": "black",
+            "font-size": "16px"
         });
     });
-    $("body").on("click",".AAFunc",function(){
+    $("body").on("click", ".AAFunc", function() {
         console.log($(this).attr("myid"));
     });
 }
